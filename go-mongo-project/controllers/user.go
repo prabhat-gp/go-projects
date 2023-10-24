7df7678d172c2fbd9ba080f7b57581b7f433ecb8
Prabhat GP <prabhat.ai20@bmsce.ac.in>
2023-10-24 10:58:47 +0530

    build-remain

--- go-mongo-project/controllers/user.go
+++ go-mongo-project/controllers/user.go
@@ -1,45 +1,108 @@
 package controllers
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
 
+	"go-mongo-project/models"
+
 	"github.com/julienschmidt/httprouter"
-	"gopkg.in/mgo.v2"
-	"gopkg.in/mgo.v2/bson"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type UserController struct {
-	session *mgo.Session
+	client *mongo.Client
 }
 
-func NewUserController(s *mgo.Session) *UserController {
-	return &UserController{s}
+func NewUserController(client *mongo.Client) *UserController {
+	return &UserController{client}
 }
 
 func (uc UserController) GetUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	id := p.ByName("id")
-	if !bson.IsObjectIdHex(id) {
+	if id == "" {
 		w.WriteHeader(http.StatusNotFound)
-	}
-
-	oid := bson.ObjectIdHex(id)
-
-	u := models.User{}
-
-	if err := uc.session.DB("go-mongo-project").C("users").FindId(oid).One(&u); err != nil {
-		w.WriteHeader(404)
 		return
 	}
 
-	uj, err := json.Marshal(u)
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	userCollection := uc.client.Database("go-mongo-project").Collection("users")
+	ctx := context.TODO()
+
+	var user models.User
+	if err := userCollection.FindOne(ctx, bson.M{"_id": oid}).Decode(&user); err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	uj, err := json.Marshal(user)
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	fmt.Printf(w, "%s\n", uj)
+	fmt.Fprintf(w, "%s\n", uj)
+}
 
+func (uc UserController) CreateUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	var user models.User
+
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	userCollection := uc.client.Database("go-mongo-project").Collection("users")
+	ctx := context.TODO()
+
+	_, err := userCollection.InsertOne(ctx, &user)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	uj, err := json.Marshal(user)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(w, "%s\n", uj)
+}
+
+func (uc UserController) DeleteUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	id := p.ByName("id")
+	if id == "" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	userCollection := uc.client.Database("go-mongo-project").Collection("users")
+	ctx := context.TODO()
+
+	_, err = userCollection.DeleteOne(ctx, bson.M{"_id": oid})
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "Deleted user ", oid, "\n")
 }
--- go-mongo-project/main.go
+++ go-mongo-project/main.go
@@ -1,5 +1,45 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"go-mongo-project/controllers"
+	"net/http"
 
+	"github.com/julienschmidt/httprouter"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	r := httprouter.New()
+	uc := controllers.NewUserController(getSession())
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+	http.ListenAndServe("localhost: 9000", r)
+}
+
+// func getSession() *mgo.Session {
+// 	s, err := mgo.Dial("mongodb://localhost:27107")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	return s
+// }
+
+func getSession() (*mongo.Client, error) {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017") // Adjust the URI as needed
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println("Connected to MongoDB!")
+	return client, nil
 }
