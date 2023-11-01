e1f38e80feeefd00373d083ed3912c64a5266f6d
Prabhat GP <prabhat.ai20@bmsce.ac.in>
2023-11-01 11:24:25 +0530

    project-complete

--- go-pexels-project/main.go
+++ go-pexels-project/main.go
@@ -4,9 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"net/http"
 	"os"
 	"strconv"
+	"time"
 )
 
 const (
@@ -36,7 +38,7 @@ type SearchResult struct {
 type Photo struct {
 	Id              int32       `json:"id"`
 	Width           int32       `json:"width"`
-	Height          int32       `json:"height`
+	Height          int32       `json:"height"`
 	Url             string      `json:"url"`
 	Photographer    string      `json:"photographer"`
 	PhotoGrapherUrl string      `json:"photographer_url"`
@@ -44,10 +46,10 @@ type Photo struct {
 }
 
 type CuratedResult struct {
-	Page     int32   `json:"page"`
-	PerPage  int32   `json:"per_page"`
-	NextPage int32   `json:""next_page`
-	Photos   []Photo `json:"photos"`
+	Page     int32       `json:"page"`
+	PerPage  int32       `json:"per_page"`
+	NextPage interface{} `json:"next_page"`
+	Photos   []Photo     `json:"photos"`
 }
 
 type PhotoSource struct {
@@ -60,6 +62,49 @@ type PhotoSource struct {
 	Square    string `json:"square"`
 	Landscape string `json:"landscape"`
 	Tiny      string `json:"tiny"`
+}
+
+type VideoSearchResult struct {
+	Page         int32   `json:"page"`
+	PerPage      int32   `json:"per_page"`
+	TotalResults int32   `json:"total_results"`
+	NextPage     string  `json:"next_page"`
+	Videos       []Video `json:"videos"`
+}
+
+type Video struct {
+	Id            int32           `json:"id"`
+	Width         int32           `json:"width"`
+	Height        int32           `json:"height"`
+	Url           string          `json:"url"`
+	Image         string          `json:"image"`
+	FullRes       interface{}     `json:"full_res"`
+	Duration      float64         `json:"duration"`
+	VideoFiles    []VideoFiles    `json:"video_files"`
+	VideoPictures []VideoPictures `json:"video_pictures"`
+}
+
+type PopularVideos struct {
+	Page         int32   `json:"page"`
+	PerPage      int32   `json:"per_page"`
+	TotalResults int32   `json:"total_results"`
+	Url          string  `json:"url"`
+	Videos       []Video `json:"videos"`
+}
+
+type VideoFiles struct {
+	Id       int32  `json:"id"`
+	Quality  string `json:"quality"`
+	FileType string `json:"file_type"`
+	Width    int32  `json:"width"`
+	Height   int32  `json:"height"`
+	Link     string `json:"link"`
+}
+
+type VideoPictures struct {
+	Id      int32  `json:"id"`
+	Picture string `json:"picture"`
+	Nr      int32  `json:"nr"`
 }
 
 func (c *Client) SearchPhotos(query string, perPage, page int) (*SearchResult, error) {
@@ -85,14 +130,14 @@ func (c *Client) CuratedPhotos(perPage, page int) (*CuratedResult, error) {
 	defer resp.Body.Close()
 	data, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return err, nil
+		return nil, err
 	}
 	var result CuratedResult
 	err = json.Unmarshal(data, &result)
 	return &result, err
 }
 
-func (c *Client) requestDoWithAuth(method, url string) (*http.Response, err) {
+func (c *Client) requestDoWithAuth(method, url string) (*http.Response, error) {
 	req, err := http.NewRequest(method, url, nil)
 	if err != nil {
 		return nil, err
@@ -113,18 +158,121 @@ func (c *Client) requestDoWithAuth(method, url string) (*http.Response, err) {
 	return resp, nil
 }
 
+func (c *Client) GetPhoto(id int32) (*Photo, error) {
+	url := fmt.Sprintf(PhotoApi+"/photos/%d", id)
+	resp, err := c.requestDoWithAuth("GET", url)
+
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	var result Photo
+	err = json.Unmarshal(data, &result)
+	return &result, err
+}
+
+func (c *Client) GetRandomPhoto() (*Photo, error) {
+	rand.Seed(time.Now().Unix())
+	randNum := rand.Intn(1001)
+	res, err := c.CuratedPhotos(1, randNum)
+	if err == nil && len(res.Photos) == 1 {
+		return &res.Photos[0], nil
+	}
+	return nil, err
+}
+
+func (c *Client) SearchVideo(query string, perPage, page int) (*VideoSearchResult, error) {
+	url := fmt.Sprintf(VideoApi+"/search?query=%s&per_page=%d&page=%d", query, perPage, page)
+	resp, err := c.requestDoWithAuth("GET", url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var result VideoSearchResult
+	err = json.Unmarshal(data, &result)
+	return &result, err
+}
+
+func (c *Client) PopularVideo(perPage, page int) (*PopularVideos, error) {
+	url := fmt.Sprintf(VideoApi+"/popular?per_page=%d&page=%d", perPage, page)
+	resp, err := c.requestDoWithAuth("GET", url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var result PopularVideos
+	err = json.Unmarshal(data, &result)
+	return &result, err
+}
+
+func (c *Client) GetRandomVideo() (*Video, error) {
+	rand.Seed(time.Now().Unix())
+	randNum := rand.Intn(1001)
+	res, err := c.PopularVideo(1, randNum)
+	if err == nil && len(res.Videos) == 1 {
+		return &res.Videos[0], nil
+	}
+	return nil, err
+}
+
+func (c *Client) GetRemainingRequestsInThisMonth() int32 {
+	return c.RemainingTimes
+}
+
+// func main() {
+// 	os.Setenv("PexelsToken", "nNifaBoJcZSskSAGcHA8T32b6O54pcyLq2B3RdBXPSlB10L2kUmKI5ww")
+// 	var TOKEN = os.Getenv("PexelsToken")
+
+// 	var c = NewClient(TOKEN)
+// 	// res, err := c.SearchPhotos("waves", 15, 1)
+// 	res, err := c.SearchVideo("waves", 15, 1)
+
+// 	if err != nil {
+// 		fmt.Printf("Search error: %v", err)
+// 	}
+// 	if res.Page == 0 {
+// 		fmt.Printf("search result wrong")
+// 	}
+// 	fmt.Println(res)
+// }
+
 func main() {
 	os.Setenv("PexelsToken", "nNifaBoJcZSskSAGcHA8T32b6O54pcyLq2B3RdBXPSlB10L2kUmKI5ww")
 	var TOKEN = os.Getenv("PexelsToken")
 
 	var c = NewClient(TOKEN)
-	res, err := c.SearchPhotos("waves")
+	res, err := c.CuratedPhotos(15, 1)
 
 	if err != nil {
-		fmt.Errorf("Search error: %v", err)
+		fmt.Printf("Search error: %v", err)
 	}
-	if res.Page == 0 {
-		fmt.Errorf("search result wrong")
+
+	// Handle the "next_page" field in the response
+	var nextPage int32
+	switch nextPageValue := res.NextPage.(type) {
+	case int:
+		nextPage = int32(nextPageValue)
+	case string:
+		nextPageInt, err := strconv.Atoi(nextPageValue)
+		if err == nil {
+			nextPage = int32(nextPageInt)
+		}
 	}
-	fmt.Println(res)
+
+	// Print the results, including the "next_page" value
+	fmt.Printf("Page: %d, PerPage: %d, NextPage: %d\n", res.Page, res.PerPage, nextPage)
 }
